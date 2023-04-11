5128e688d0d0af082128076e1719415d0cf9984e
Rheiana Cuevas <rmcuevas@ucdavis.edu>
2023-04-11 00:52:17 -0700

    finished portal page

--- home-is/script.js
+++ home-is/script.js
@@ -5,6 +5,11 @@
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
+    const buttonIcon = document.querySelector('button img');
+    const bannerTitle = document.querySelector('#banner h3');
+    const bannerSubtitle = document.querySelector('#banner p');
+    const fallBanner = document.querySelector('#leaf-animation');
+    const springBanner = document.querySelector('#flower-animation');
     let mode = 'dark';
 
     button.addEventListener('click', function() {
@@ -12,6 +17,11 @@
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
+            buttonIcon.src = 'images/flower.png';
+            bannerTitle.innerHTML = `"Until the spring day comes again"`;
+            bannerSubtitle.innerHTML = '-Spring Day by BTS';
+            fallBanner.className = 'hidden';
+            springBanner.className = 'showing'
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -20,6 +30,11 @@
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
+            buttonIcon.src = 'images/fall-switch.png';
+            bannerTitle.innerHTML = `"Autumn leaves falling down like pieces into place"`;
+            bannerSubtitle.innerHTML = "-All Too Well(10 Minute Version)(Taylor's Version)";
+            fallBanner.className = 'showing';
+            springBanner.className = 'hidden'
             for (const section of sections) {
                 section.removeAttribute('class');
             }
