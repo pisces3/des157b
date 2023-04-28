fce271733e0f6fe15c1999139847f413442f0b60
Rheiana Cuevas <rmcuevas@ucdavis.edu>
2023-04-27 22:55:33 -0700

    finished studio 2!

--- studio2/script.js
+++ studio2/script.js
@@ -2,16 +2,53 @@
     'use strict';
     console.log('reading js');
 
-    const data = document.querySelector('#date');
+    let globalData;
+    let numDataPoints;
+
+    const date = document.querySelector('#date');
+    const time = document.querySelector('#time');
+    const colorBG = document.querySelector('body');
+    const wallpaper = document.querySelector('#wallpaper');
+    const word = document.querySelector('#word');
 
     async function getData() {
-        const screenTime = await fetch('data/data.json');
-        const data = await screenTime.json();
-        console.log(data);
+        const timeTaken = await fetch('data/data.json');
+        const data = await timeTaken.json();
+        // console.log(data);
+        const dataPoints = Object.keys(data);
+        // Gets the value and puts in the globalData array
+        globalData = Object.values(data);
+        // gets number of entries in JSON
+        numDataPoints = dataPoints.length;
     }
 
-    function getDate(data) {
-        let html = '';
+    function showTime(point, data) {
+        // console.log(data[point]);
+        date.innerHTML = `${data[point].day}, ${data[point].date}`;
+        time.innerHTML = data[point].time_taken;
+        colorBG.style.backgroundColor = data[point].color;
+        wallpaper.src = data[point].wallpaper;
+        word.innerHTML = data[point].word;
+    }
+
+    document.addEventListener('mousemove', reportPos);
+
+    let prevLoc = 0;
+
+    function reportPos(event) {
+        const windowSize = window.innerWidth;
+        //The window needs to be divided into sections for each time in the JSON data
+        const timeSection = windowSize / numDataPoints;
+        const xPos = event.clientX;
+        // changeTime will be a number from 0-16
+        const changeTime = Math.floor(xPos / timeSection);
+
+        // When you move the mouse into the next segment, change the interface.
+        if (changeTime !== prevLoc) {
+            //console.log(changeTime);
+            showTime(changeTime, globalData);
+            prevLoc = changeTime;
+        }
     }
 
     getData();
